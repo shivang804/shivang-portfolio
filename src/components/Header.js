c44b18e1881a80ffa682dd22bc3fd97f289cdbb4
shivang804 <110419934+shivang804@users.noreply.github.com>
2024-08-23 14:15:58 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ function Header() {
             <ul>
                 <li><a href='#about'>About</a></li>
                 <li><a href='#skills'>Skills</a></li>
+                <li><a href='#hobbies'>Hobbies</a></li>
+                <li><a href='#education'>Education</a></li>
                 <li><a href='#experience'>Experience</a></li>
                 <li><a href='#projects'>Projects</a></li>
-                <li><a href='#education'>Education</a></li>
-                <li><a href='#hobbies'>Hobbies</a></li>
                 <li><a href='mailto:sharmashivang804@gmail.com'>Contact</a></li>
                 <li><a href='/resume.pdf' downloada>Resume</a></li>
             </ul>
